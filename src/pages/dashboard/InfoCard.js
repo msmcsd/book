7c2596a5a9a71210ec86dd8bf036421c72b3405e
Jason Shih <jason_shih@hotmail.com>
2022-09-30 15:01:06 -0700

    Add info card

--- src/pages/dashboard/InfoCard.js
+++ src/pages/dashboard/InfoCard.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { NumericFormat } from "react-number-format";
-import { Box } from "@mui/material";
+import { Box, Paper } from "@mui/material";
 import Typography from "@mui/material/Typography";
 
 export default function InfoCard({ name, value, icon }) {
   return (
-    <Box
-      sx={{ display: "flex", borderRadius: 2, boxShadow: 2, p: 2, width: 180 }}
+    <Paper
+      elevation={3}
+      sx={{ display: "flex", p: 2, width: 180 }}
+      // sx={{ display: "flex", borderRadius: 2, boxShadow: 2, p: 2, width: 180 }}
     >
       {icon}
-      <Box sx={{ textAlign: "center" }}>
+      <Box
+        sx={{
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Typography>{name}</Typography>
         <Typography variant="h4" sx={{ mt: 2 }}>
           {!isNaN(+value) ? (
@@ -24,6 +32,6 @@ export default function InfoCard({ name, value, icon }) {
           )}
         </Typography>
       </Box>
-    </Box>
+    </Paper>
   );
 }
